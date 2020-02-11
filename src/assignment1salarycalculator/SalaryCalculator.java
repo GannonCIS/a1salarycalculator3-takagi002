f82da183f55ec976d4391ef379052359e38d2e5d
Julia Takagi <takagi002@gannon.edu>
2020-02-11 14:53:38 -0500

    complete method prototypes

---
+++ src/assignment1salarycalculator/SalaryCalculator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+/**
+ *
+ * @author julia
+ */
+public class SalaryCalculator {
+    
+    public void SalaryCalculator(){}
+    
+    public double calcHourlyWage(int salary){
+        
+    }
+    
+    public double calcSalary(int hourlyWage){
+        
+    }
+    
+    public double calcTaxes(int perc){
+        
+    }
+    
+    public double monthlyAfterTax(int salary, double perc){
+        
+    }
+}
